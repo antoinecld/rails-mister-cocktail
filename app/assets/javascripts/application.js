f03ca71c2cecdbcae1762d833ad081343a076673
Antoine Coulaud <antoinecoulaud@outlook.com>
2017-02-18 10:15:48 +0100

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery-fileupload/basic
+
 //= require cloudinary/jquery.cloudinary
 //= require_tree .
 
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,5 +4,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   mount_uploader :photo, PhotoUploader
   validates :photo, presence: true
-  has_attachment :photo
+  # has_attachment :photo
 end
